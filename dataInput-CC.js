14063ccdaa3e7faa5a46c6c65bb3a19901b4699a
JustinHu <justinhu@JustinHudeiMac.local>
2017-10-30 20:09:37 +0800

    add

--- dataInput-CC.js
+++ dataInput-CC.js
@@ -3,10 +3,11 @@
  * @type {{math: number, economics: number, computer: number, physics: number, physicalExercise: number, english: {hearing: number, reading: number, seeing: number, writing: number, grammar: number, word: number, speaking: number, value: number}}}
  */
 // 本周目标设定
-let weekScore = {math:100, economics:50, computer:320, physics:10, physicalExercise:24,
+let weekScore = {math:100, economics:50, computer:320, physics:20, physicalExercise:24,
     english:{hearing:0,reading:0,seeing:0,writing:0,grammar:0,word:0,speaking:0,value:100}};
 // 礼物列表
 let giftList=[
+    {name:"SlamDunk Comic", value:228},
     {name:"Air Jordan 6 Sport Blue", value:1399},
     {name:"ipad pro 12.0", value:8376},
     {name:"Apple watch", value:3288},
