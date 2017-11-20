9506815aa36f60d195eb03b4219e5cff4664a8b1
JustinHu <justinhu@JustinHudeiMac.local>
2017-11-20 14:53:55 +0800

    add

--- dataInput-CC.js
+++ dataInput-CC.js
@@ -18,6 +18,22 @@ let giftList=[
 ];
 // 每日学习时间记录
 let dataArr=[
+	{   year:2017,month:11,date:19,
+        math:0, economics:0, computer:0, physics:0, physicalExercise:0,
+        english:{hearing:0,reading:0,seeing:0,writing:0,grammar:0,speaking:0,word:0,value:0},
+    },
+	{   year:2017,month:11,date:18,
+        math:0, economics:0, computer:0, physics:0, physicalExercise:0,
+        english:{hearing:0,reading:0,seeing:0,writing:0,grammar:0,speaking:0,word:0,value:0},
+    },
+	{   year:2017,month:11,date:17,
+        math:0, economics:0, computer:0, physics:0, physicalExercise:1,
+        english:{hearing:0,reading:0,seeing:0,writing:0,grammar:0,speaking:0,word:0,value:0},
+    },
+	{   year:2017,month:11,date:16,
+        math:0, economics:0, computer:10, physics:0, physicalExercise:1,
+        english:{hearing:0,reading:10,seeing:0,writing:0,grammar:0,speaking:0,word:0,value:0},
+    },
     {   year:2017,month:11,date:15,
         math:12, economics:0, computer:32, physics:0, physicalExercise:1,
         english:{hearing:0,reading:32,seeing:0,writing:0,grammar:9,speaking:0,word:0,value:0},
