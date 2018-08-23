c8d33a8d06ffef10ca9bce3945867c4c1e07c77a
unknown <hujinchao@iqiyi.pps>
2018-08-23 10:05:41 +0800

    add

--- dataInput-CC.js
+++ dataInput-CC.js
@@ -23,6 +23,14 @@ let giftList=[
 
 // Daily study time records
 let dataArr=[
+    {   year:2018,month:8,date:22,
+        math:0, economics:0, computer:40, physics:0, physicalExercise:0,
+        english:{hearing:0,reading:0,seeing:0,writing:0,grammar:0,speaking:0,word:0,value:0},
+    },
+    {   year:2018,month:8,date:21,
+        math:0, economics:0, computer:49, physics:0, physicalExercise:10,
+        english:{hearing:0,reading:0,seeing:0,writing:0,grammar:0,speaking:0,word:0,value:0},
+    },
     {   year:2018,month:8,date:20,
         math:0, economics:0, computer:68, physics:0, physicalExercise:4,
         english:{hearing:0,reading:0,seeing:0,writing:0,grammar:0,speaking:5,word:0,value:0},
